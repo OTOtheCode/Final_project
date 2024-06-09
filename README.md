672db628e6473916e6e1ab76af4e2dc73566147b
OTOtheCode <okharatishvili@xnet.ge>
2024-06-09 23:52:39 +0400

    added operation file. and coresponding code for services to show up in operations

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
-with this code We try to achive simple functionality for managing clinics. It has console based user interface. 
-Program uses fixed data_structure. all information is saved in .csv files(patients.csv, staff.csv, services.csv and etc.)
-When starting program it promts you to select one of the main fields. a) registration desk menu. b)clinic management manu. c) accountant field. after it lets you in the coresponding menus.
-Don't hesitate to try it out. 
+Hello everyone.
+Here is our final project. Hospital's management consolar application. 
+By interacting with code through console operator can chose what organizational unit he/she wants to enter:
+1. Registration desk
+2. Clinic Management dashboard
+3. Accounting dashboard.
+
+Each of these has dedicated functions:
+1. from Registrarion desk dashboard operator can register patients, view history, schaldule meeting with doctor, select services for patients etc.
+2. from clinic management dashboard. operator can monitor staff operations, view personal on duty, add/remove staff members, create and assigne task to staff memeber.
+3. from accouting dashboard. accountatns can gather infromation about salaries, procedures, costs, monthely incomes etc. 
--- services.py
+++ services.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from insurance import Insurance
 from datetime import datetime
-
+import os
 class AllServices:
     def __init__(self):
         pass
@@ -11,7 +11,7 @@ class AllServices:
         selected_services = []
         service_file = 'services.csv'
         patient_file = 'patients.csv'
-        
+        operations_file = 'operations.csv'
         #პაციენტის აიდის შეყვანა
         patient_id = input("Please enter the patient's ID: ").strip()  
         
@@ -79,8 +79,28 @@ class AllServices:
         
         
         patients_df.to_csv(patient_file, sep=';', index=False)
-        
-        print("Patient history updated successfully.")
+       
+        #ვამატებთ ინფორმაციას გაწეული მომსახურების შესახებ კლინიკის ოპერაციების ჟურნალში
+        operations = []
+        for selected_service in selected_services:
+            operation_entry = {
+                'timestamp': timestamp,
+                'service': selected_service['services'],
+                'cost': selected_service['cost']
+            }
+            operations.append(operation_entry)
+
+        operations_df = pd.DataFrame(operations)
+
+        if os.path.exists(operations_file) and os.path.getsize(operations_file) > 0:
+            existing_operations_df = pd.read_csv(operations_file, delimiter=';')
+            combined_operations_df = pd.concat([existing_operations_df, operations_df], ignore_index=True)
+        else:
+            combined_operations_df = operations_df
+
+        combined_operations_df.to_csv(operations_file, sep=';', index=False)
+
+        print("Patient history and operations updated successfully.")
 
 
 
